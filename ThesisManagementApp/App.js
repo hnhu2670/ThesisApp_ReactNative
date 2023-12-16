003088445f2cc96e2625bcf0d2f30c7e53c90a2b
hnhu2670 <2051050327nhu@ou.edu.vn>
2023-12-16 10:50:07 +0700

    update

--- ThesisManagementApp/App.js
+++ ThesisManagementApp/App.js
@@ -1,26 +1,31 @@
 import * as React from 'react';
+import 'react-native-gesture-handler';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './component/Home';
-import Login from './component/Login'
-// import Register from './component/Register';
+import { createContext } from 'react';
+import MyUserReducer from './src/reducers/MyUserReducer';
+import { useReducer } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import Home from './src/component/home/Home';
+import Login from './src/login/Login';
+
+export const MyUserContext = createContext();
 
 const Stack = createNativeStackNavigator();
-
 function App() {
+  const [user, dispatch] = useReducer(MyUserReducer, AsyncStorage.getItem("user") || null)
   return (
-
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Quá mệt mõi" component={Home} />
-        <Stack.Screen name="Login" component={Login} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
-
-
-  );
+    <MyUserContext.Provider value={[user, dispatch]}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Login'>
+          <Stack.Screen name='Home' component={Home} />
+          <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </MyUserContext.Provider>
+  )
 }
 
 export default App;
---
+++ ThesisManagementApp/src/styles/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+// css
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: "#2d665f"
+    },
+    fontColor: {
+        color: "white"
+    },
+    color: {
+        color: "#2d665f"
+    }
+});
+export default styles
